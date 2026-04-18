ffe1ad05a316c3c0b6de4131cdd1a24835679ca2
Silencer <silencer2k@gmail.com>
2026-04-19 00:02:18 +0300

    Fix tools paths

--- scripts/extract-maps.sh
+++ scripts/extract-maps.sh
@@ -7,7 +7,7 @@ cd "$BASE_DIR/data"
 rm -rf dbc maps Cameras Buildings vmaps mmaps
 mkdir -p Buildings vmaps mmaps
 
-./map_extractor
-./vmap4_extractor
-./vmap4_assembler Buildings vmaps
-./mmaps_generator
+"$AC_CODE_DIR/env/dist/bin/map_extractor"
+"$AC_CODE_DIR/env/dist/bin/vmap4_extractor"
+"$AC_CODE_DIR/env/dist/bin/vmap4_assembler" Buildings vmaps
+"$AC_CODE_DIR/env/dist/bin/mmaps_generator"
